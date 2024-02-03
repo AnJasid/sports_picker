1882032fa80eb142f2bb099a9d8f1a8e31e0706c
Joel <86185696+AnJasid@users.noreply.github.com>
2024-02-03 20:54:06 +0800

    update home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -59,6 +59,7 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        automaticallyImplyLeading: false,
         elevation: 0,
         // backgroundColor: const Color.fromARGB(255, 236, 226, 187),
         title: const Text(
