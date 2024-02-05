5946ce3dc000c4ad809bffc6a35e7e18cdae2d8e
Joel <86185696+AnJasid@users.noreply.github.com>
2024-02-05 14:37:46 +0800

    update for hobby data

--- lib/data/hobby_data.dart
+++ lib/data/hobby_data.dart
@@ -1,78 +1,93 @@
 import 'package:sports_picker/model/hobby_model.dart';
 
 const hobbyDetails = [
-  HobbyModel(
-    hobbyTitle: 'Cricket',
-    hobbyDesc:
+  SportModel(
+    sportImage: '',
+    sportTitle: 'Cricket',
+    sportDesc:
         'Cricket is a highly popular sport in India, deeply ingrained in the country\'s culture. Played with a bat and ball, the game involves two teams competing to score runs by hitting the ball and running between wickets. Cricket has three main formats: Test matches, One Day Internationals (ODIs), and Twenty20 (T20) matches. The sport\'s apex in India is the Indian Premier League (IPL), a T20 tournament featuring franchises representing different cities. Cricket transcends boundaries, uniting millions of fans across the nation, and iconic players like Sachin Tendulkar, Virat Kohli, and MS Dhoni have achieved legendary status. The sport\'s influence extends beyond the field, impacting lifestyle, conversations, and national pride, making cricket an integral part of India\'s sporting identity.',
   ),
-  HobbyModel(
-    hobbyTitle: 'Field Hockey',
-    hobbyDesc:
+  SportModel(
+    sportImage: '',
+    sportTitle: 'Field Hockey',
+    sportDesc:
         'Field hockey is a sport deeply rooted in India\'s sporting history and has garnered significant national acclaim. Played with a small, hard ball and a curved stick, two teams aim to score by hitting the ball into the opponent\'s goal. India has a glorious legacy in field hockey, having won multiple Olympic gold medals during the mid-20th century. The sport\'s popularity remains, with a passionate fan base and ongoing domestic competitions. Despite cricket\'s dominance, field hockey continues to be cherished, especially for its historical significance and the pride associated with India\'s past achievements in international tournaments.',
   ),
-  HobbyModel(
-    hobbyTitle: 'Football (Soccer)',
-    hobbyDesc:
+  SportModel(
+    sportImage: '',
+    sportTitle: 'Football (Soccer)',
+    sportDesc:
         'Football, known as soccer in some regions, has seen a notable surge in popularity in India. The sport, played with a round ball, involves two teams aiming to score goals by propelling the ball into the opposing team\'s net. India has witnessed an increased interest in football, thanks in part to the Indian Super League (ISL), a professional football league that has elevated the sport\'s status. The ISL has attracted international players and created a fervent following among fans. While cricket remains the dominant sport, football has carved its niche, capturing the hearts of enthusiasts across the nation and fostering a new generation of football lovers. The grassroots development and the emergence of local talent signal a promising future for football in India.',
   ),
-  HobbyModel(
-    hobbyTitle: 'Badminton',
-    hobbyDesc:
+  SportModel(
+    sportImage: '',
+    sportTitle: 'Badminton',
+    sportDesc:
         'Badminton has become a widely popular sport in India, characterized by its fast-paced and dynamic nature. Played with a shuttlecock, the game involves two or four players who take turns hitting the shuttlecock over the net, aiming to score points. In recent years, India has produced exceptional badminton players who have achieved success on the international stage, contributing to the sport\'s surge in popularity. Notable players like P.V. Sindhu, Saina Nehwal, and Kidambi Srikanth have inspired a growing number of enthusiasts. Badminton has transitioned from a casual backyard game to a professional sport in India, attracting significant attention and fostering a competitive spirit among aspiring players across the country.',
   ),
-  HobbyModel(
-    hobbyTitle: 'Kabaddi',
-    hobbyDesc:
+  SportModel(
+    sportImage: '',
+    sportTitle: 'Kabaddi',
+    sportDesc:
         'Kabaddi, deeply rooted in India\'s cultural heritage, has experienced a remarkable resurgence in popularity, particularly with the advent of the Pro Kabaddi League (PKL). This indigenous sport involves two teams, with a "raider" crossing into the opponent\'s territory to tag as many defenders as possible without taking a breath, all while chanting "kabaddi, kabaddi." The defenders aim to stop the raider and prevent them from returning to their half. The PKL has played a pivotal role in elevating kabaddi\'s status, drawing large audiences and showcasing top talents. The league has not only revitalized interest in the traditional sport but has also created sporting icons, turning kabaddi into a thrilling and widely appreciated spectacle across the nation.',
   ),
-  HobbyModel(
-    hobbyTitle: 'Wrestling',
-    hobbyDesc:
+  SportModel(
+    sportImage: '',
+    sportTitle: 'Wrestling',
+    sportDesc:
         'Wrestling holds a significant place in India\'s sports landscape, characterized by its rich history and strong cultural ties. The sport involves physical combat where two competitors grapple with each other, aiming to gain control and score points. India has a proud tradition in wrestling, with numerous athletes achieving success in international competitions, including the Olympics. The sport has deep grassroots connections, with many wrestlers emerging from small towns and villages. The country\'s love for wrestling is evident in the passionate following it enjoys, and events like the Indian Wrestling League have further contributed to its popularity. Wrestling not only showcases the strength and skill of its participants but also embodies the resilience and determination that resonate with fans throughout the nation.',
   ),
-  HobbyModel(
-    hobbyTitle: 'Tennis',
-    hobbyDesc:
+  SportModel(
+    sportImage: '',
+    sportTitle: 'Tennis',
+    sportDesc:
         'Tennis has found a strong and growing fan base in India, propelled by the success of Indian players on the international stage and increased accessibility to the sport. Played with a racket and a ball, tennis involves singles or doubles matches where players aim to score points by hitting the ball over the net and into the opponent\'s court. India has produced tennis legends like Leander Paes, Mahesh Bhupathi, and Sania Mirza, who have achieved Grand Slam victories and brought global recognition to Indian tennis. The sport is popular not only for its competitive nature but also for its inclusive appeal, encouraging participation across age groups and skill levels. Tennis tournaments, both domestic and international, captivate audiences and contribute to the sport\'s rising popularity across the country.',
   ),
-  HobbyModel(
-    hobbyTitle: 'Athletics',
-    hobbyDesc:
+  SportModel(
+    sportImage: '',
+    sportTitle: 'Athletics',
+    sportDesc:
         'Athletics, encompassing a wide range of track and field events, holds a special place in India\'s sporting landscape. The sport involves disciplines like running, jumping, throwing, and combined events. India has produced notable athletes who have excelled in international competitions, showcasing prowess in events such as sprinting, long-distance running, javelin throw, and more. While cricket dominates the sports scene, athletics has gained prominence with athletes like Milkha Singh, P.T. Usha, and Neeraj Chopra making significant contributions. The sport has a grassroots presence, with schools and colleges promoting track and field events. Athletics embodies the spirit of competition and athleticism, contributing to India\'s representation in the global sporting arena',
   ),
-  HobbyModel(
-    hobbyTitle: 'Boxing',
-    hobbyDesc:
+  SportModel(
+    sportImage: '',
+    sportTitle: 'Boxing',
+    sportDesc:
         'Boxing has emerged as a formidable and widely followed sport in India, gaining popularity for its raw intensity and individual brilliance. In boxing, two competitors square off, aiming to score points by landing punches on each other while avoiding their opponent\'s strikes. India has produced accomplished boxers who have made their mark in various weight categories on the international stage. The sport\'s rising popularity is attributed to the success of Indian boxers like Mary Kom, Vijender Singh, and Shiva Thapa, who have not only achieved accolades but have also inspired a new generation of aspiring pugilists. Boxing has transcended traditional boundaries, capturing the imagination of fans and contributing significantly to India\'s presence in global sporting events.',
   ),
-  HobbyModel(
-    hobbyTitle: 'Shooting',
-    hobbyDesc:
+  SportModel(
+    sportImage: '',
+    sportTitle: 'Shooting',
+    sportDesc:
         'Shooting has gained prominence as a precision sport in India, marked by meticulous aim, focus, and accuracy. The sport involves competitors using firearms to hit targets with precision, testing their marksmanship skills across various disciplines. India has witnessed notable success in shooting, with athletes like Abhinav Bindra, Gagan Narang, and Anjali Bhagwat achieving Olympic and World Championship medals. The sport has attracted attention for its technical nuances and mental discipline. Shooting has become a popular pursuit among Indian athletes, with an increasing number of enthusiasts participating in competitions at the national and international levels. The success of Indian shooters has not only brought accolades but has also contributed to the sport\'s growing popularity and recognition in the country.',
   ),
-  HobbyModel(
-    hobbyTitle: 'Chess',
-    hobbyDesc:
+  SportModel(
+    sportImage: '',
+    sportTitle: 'Chess',
+    sportDesc:
         'Chess is a cerebral and strategic sport that has captured the imagination of enthusiasts in India, transcending age and background. Played on a checkered board, the game involves two players maneuvering pieces with distinct moves to outsmart and checkmate their opponent\'s king. India has produced several Grandmasters, including Viswanathan Anand, who became a World Chess Champion, elevating the country\'s status in the chess world. Chess is widely played in schools, colleges, and communities, fostering critical thinking, concentration, and strategic planning. The sport\'s popularity extends beyond competitive play, with numerous chess clubs, events, and online platforms contributing to a thriving chess culture in India. Chess embodies the essence of intellect and has become a symbol of strategic prowess and mental acuity in the country.',
   ),
-  HobbyModel(
-    hobbyTitle: 'Weightlifting',
-    hobbyDesc: '',
+  SportModel(
+    sportImage: '',
+    sportTitle: 'Weightlifting',
+    sportDesc: '',
   ),
-  HobbyModel(
-    hobbyTitle: 'Basketball',
-    hobbyDesc:
+  SportModel(
+    sportImage: '',
+    sportTitle: 'Basketball',
+    sportDesc:
         'Weightlifting, a discipline demanding strength, technique, and precision, has gained recognition and a dedicated following in India. In weightlifting, athletes compete to lift maximum weights in two main categories: the snatch and the clean and jerk. India has produced notable weightlifters who have excelled in international competitions, winning medals and establishing records. The sport has a grassroots presence, with many lifters emerging from diverse backgrounds, showcasing their prowess at the national and global levels. Weightlifting has garnered attention for its raw power and displays of physical strength, contributing to its popularity among fans and fitness enthusiasts alike. The success of Indian weightlifters continues to inspire a new generation, making weightlifting a significant and respected sport within the country\'s sporting landscape.',
   ),
-  HobbyModel(
-    hobbyTitle: 'Golf',
-    hobbyDesc:
+  SportModel(
+    sportImage: '',
+    sportTitle: 'Golf',
+    sportDesc:
         'Golf, known for its precision and elegance, has carved a niche for itself in India, attracting enthusiasts and players from various walks of life. Played on lush greens with the aim of completing a course in the fewest strokes possible, golf has seen a surge in popularity with the emergence of talented Indian golfers. Players like Jeev Milkha Singh, Anirban Lahiri, and Aditi Ashok have represented India on the international golfing stage, bringing recognition to the country. Golf courses, both traditional and modern, are spread across the nation, offering a serene backdrop for the sport. Golf has become more accessible, with a growing number of golf clubs and tournaments fostering interest and participation. The sport embodies a unique blend of skill, strategy, and a connection with nature, making it an increasingly popular choice among sports enthusiasts in India.',
   ),
-  HobbyModel(
-    hobbyTitle: 'Table Tennis',
-    hobbyDesc:
+  SportModel(
+    sportImage: '',
+    sportTitle: 'Table Tennis',
+    sportDesc:
         'Table tennis, a fast-paced and dynamic sport, has gained significant popularity in India. Played on a rectangular table with a net across the middle, the game involves players using paddles to hit a lightweight ball back and forth. India has produced accomplished table tennis players who have excelled at the national and international levels. The sport has a widespread appeal, with enthusiasts playing in schools, colleges, and community centers. With the emergence of top players like Achanta Sharath Kamal and Manika Batra, table tennis has garnered attention and a growing fan base. The sport\'s accessibility, quick pace, and tactical nature contribute to its popularity, making it a favorite among players of all ages and skill levels in India.',
   ),
 ];
