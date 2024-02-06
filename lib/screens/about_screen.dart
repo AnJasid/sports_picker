0608af4ee9154ebc36df3c4550465ea245d9dd47
Joel <86185696+AnJasid@users.noreply.github.com>
2024-02-06 18:30:58 +0800

    update about screen

--- lib/screens/about_screen.dart
+++ lib/screens/about_screen.dart
@@ -6,6 +6,7 @@ class AboutScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 25),
       width: double.infinity,
       height: double.infinity,
       decoration: const BoxDecoration(
@@ -19,7 +20,42 @@ class AboutScreen extends StatelessWidget {
         ),
       ),
       child: Center(
-        child: Container(),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'About',
+              style: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            const SizedBox(height: 50),
+            Container(
+              padding: const EdgeInsets.all(5),
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: const Color(0xff283618),
+                borderRadius: BorderRadius.circular(30),
+              ),
+              child: Container(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 20,
+                  vertical: 20,
+                ),
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: const Color(0xfffefae0),
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                child: const Text(
+                  '10Circ - Sport Opt Picker is a mobile application designed to present a comprehensive catalogue of information regarding various popular sports in India. Additionally, this application boasts a distinctive feature aimed at facilitating users in selecting a sport suited to their interests, providing opportunities for leisure and potential hobby pursuits. The current iteration of the app is version 1.0.0.',
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
