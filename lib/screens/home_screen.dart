acfd1c851cfe8fcb3549db74bd805fce6033516c
Joel <86185696+AnJasid@users.noreply.github.com>
2024-02-06 13:56:34 +0800

    update home screen UI

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -65,124 +65,140 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 
-  void _showPopupMenu(BuildContext context) {
-    showMenu(
-      context: context,
-      position: const RelativeRect.fromLTRB(0, 56, 0, 0),
-      items: [
-        PopupMenuItem(
-          onTap: () => _goStartScreen(),
-          value: 'home',
-          child: const Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(Icons.home),
-              SizedBox(width: 6),
-              Text(
-                'Home',
-                style: TextStyle(
-                  fontSize: 18,
-                ),
-              ),
-            ],
-          ),
-        ),
-        const PopupMenuItem(
-          value: 'about',
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(Icons.info),
-              SizedBox(width: 6),
-              Text(
-                'About',
-                style: TextStyle(
-                  fontSize: 18,
-                ),
-              ),
-            ],
-          ),
-        ),
-      ],
-    );
-  }
+  // void _showPopupMenu(BuildContext context) {
+  //   showMenu(
+  //     context: context,
+  //     position: const RelativeRect.fromLTRB(0, 56, 0, 0),
+  //     items: [
+  //       PopupMenuItem(
+  //         onTap: () => _goStartScreen(),
+  //         value: 'home',
+  //         child: const Row(
+  //           mainAxisAlignment: MainAxisAlignment.center,
+  //           children: [
+  //             Icon(Icons.home),
+  //             SizedBox(width: 6),
+  //             Text(
+  //               'Home',
+  //               style: TextStyle(
+  //                 fontSize: 18,
+  //               ),
+  //             ),
+  //           ],
+  //         ),
+  //       ),
+  //       const PopupMenuItem(
+  //         value: 'about',
+  //         child: Row(
+  //           mainAxisAlignment: MainAxisAlignment.center,
+  //           children: [
+  //             Icon(Icons.info),
+  //             SizedBox(width: 6),
+  //             Text(
+  //               'About',
+  //               style: TextStyle(
+  //                 fontSize: 18,
+  //               ),
+  //             ),
+  //           ],
+  //         ),
+  //       ),
+  //     ],
+  //   );
+  // }
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Container(
-        padding: const EdgeInsets.all(40),
-        width: double.infinity,
-        height: double.infinity,
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-              image: AssetImage('assets/homeBG.png'), fit: BoxFit.cover),
+    return Container(
+      padding: const EdgeInsets.all(40),
+      width: double.infinity,
+      height: double.infinity,
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: [
+            Color(0xfffefae0),
+            Color(0xffbc6c25),
+          ],
         ),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Align(
-                alignment: Alignment.topLeft,
-                child: GestureDetector(
-                  onTap: () => _showPopupMenu(context),
-                  child: Container(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 15,
-                      vertical: 5,
-                    ),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      color: const Color(0xff28341c),
-                    ),
-                    child: const Icon(
-                      Icons.menu,
-                      color: Colors.white,
-                    ),
-                  ),
+      ),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Spacer(flex: 1),
+            const Text(
+              'Shuffle Sports',
+              style: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            // Align(
+            //   alignment: Alignment.topLeft,
+            //   child: GestureDetector(
+            //     onTap: () => _showPopupMenu(context),
+            //     child: Container(
+            //       padding: const EdgeInsets.symmetric(
+            //         horizontal: 15,
+            //         vertical: 5,
+            //       ),
+            //       decoration: BoxDecoration(
+            //         borderRadius: BorderRadius.circular(20),
+            //         color: const Color(0xff28341c),
+            //       ),
+            //       child: const Icon(
+            //         Icons.menu,
+            //         color: Colors.white,
+            //       ),
+            //     ),
+            //   ),
+            // ),
+            const SizedBox(height: 50),
+            Container(
+              padding: const EdgeInsets.all(20),
+              height: 300,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                gradient: const LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    Color(0xfffefae0),
+                    Color(0xff606c38),
+                  ],
                 ),
               ),
-              const Spacer(flex: 1),
-              Container(
-                padding: const EdgeInsets.all(20),
-                height: 300,
+              child: Container(
+                height: double.infinity,
                 width: double.infinity,
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(30),
-                  image: const DecorationImage(
-                    image: AssetImage('assets/squareBG1.png'),
-                    fit: BoxFit.cover,
-                  ),
+                  color: const Color(0xff28341c),
                 ),
-                child: Container(
-                  height: double.infinity,
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(30),
-                    color: const Color(0xff28341c),
-                  ),
-                  child: Center(
-                    child: Text(
-                      currentHobby.sportTitle,
-                      style: const TextStyle(
-                        color: Colors.white,
-                        fontSize: 20,
-                      ),
+                child: Center(
+                  child: Text(
+                    currentHobby.sportTitle,
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
                     ),
                   ),
                 ),
               ),
-              const Spacer(flex: 2),
-              AppButton(
-                onTap: () {
-                  if (!isShuffling) {
-                    startShuffle();
-                  }
-                },
-                btnText: 'Reshuffle',
-              ),
-            ],
-          ),
+            ),
+            const Spacer(flex: 2),
+            AppButton(
+              onTap: () {
+                if (!isShuffling) {
+                  startShuffle();
+                }
+              },
+              btnText: 'Reshuffle',
+            ),
+          ],
         ),
       ),
     );
