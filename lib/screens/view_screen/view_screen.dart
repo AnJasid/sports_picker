1aa6152514e0791f197b2574156ce60330990be3
Joel <86185696+AnJasid@users.noreply.github.com>
2024-02-06 14:43:21 +0800

    update for view screen UI

--- lib/screens/view_screen/view_screen.dart
+++ lib/screens/view_screen/view_screen.dart
@@ -6,19 +6,42 @@ class ViewScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Container(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 10,
-          vertical: 10,
+    return Container(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 10,
+        vertical: 5,
+      ),
+      width: double.infinity,
+      height: double.infinity,
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            Color(0xfffefae0),
+            Color(0xff606c38),
+          ],
         ),
-        width: double.infinity,
-        height: double.infinity,
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-              image: AssetImage('assets/viewBG.png'), fit: BoxFit.cover),
+      ),
+      child: Center(
+        child: Column(
+          children: [
+            const SizedBox(height: 80),
+            const Text(
+              'Popular Sports in India',
+              style: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            const SizedBox(height: 20),
+            Container(
+              width: double.infinity,
+              height: 600,
+              child: const SportList(),
+            )
+          ],
         ),
-        child: const SportList(),
       ),
     );
   }
