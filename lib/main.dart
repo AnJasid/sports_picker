d5f765d5e2eb1c0e0f5bc6365a2750f24c55c2bb
Joel <86185696+AnJasid@users.noreply.github.com>
2024-02-07 13:42:58 +0800

    update for main

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,30 @@
 import 'package:flutter/material.dart';
 
 import 'package:sports_picker/screens/start_screen.dart';
+import 'package:onesignal_flutter/onesignal_flutter.dart';
+import 'package:flutter/services.dart';
 
 void main() {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  OneSignal.initialize("174b755e-9043-449f-975c-5e7a643898f6");
+  OneSignal.Notifications.requestPermission(true);
+
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
+  runApp(const SportPicker());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class SportPicker extends StatelessWidget {
+  const SportPicker({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
+      title: '10Circ - Sport Opt Picker',
       home: StartScreen(),
     );
   }
